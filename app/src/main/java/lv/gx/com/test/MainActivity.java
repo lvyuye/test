6444963980a02f87839e31b2f8b5e26481bc6ed7
lvyuye <2631607594@qq.com>
2017-02-22 09:13:02 +0800

    sdasdaxzxas

--- app/src/main/java/lv/gx/com/test/MainActivity.java
+++ app/src/main/java/lv/gx/com/test/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     int a = 1;
+    String b = "asda";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
